ba3f481a74bcf7164d174a9c35331c0cdf170a9e
Reo Sadahira <s246116@wakayama.u.ac.jp>
2023-11-14 16:22:33 +0900

    空白ごとにカンマを入れるCSV

--- text_to_csv.py
+++ text_to_csv.py
@@ -1,11 +1,16 @@
 import csv
 
-# juniperのconfigを読み込む
+# configを読み込む
 with open('config/juniper_config.txt', 'r') as config_txt:
-  #各行を配列linesに格納
-  lines = config_txt.readlines()
-  print(lines)
+	# 各行を配列linesに格納
+	lines = config_txt.readlines()
+	# print(lines)
 
 # テキストファイルからCSVファイルに変換
-# def text_to_csv():
-#   config_csv = data.replace("txt", "csv")
+with open('config/juniper_config.csv', 'w', newline='') as config_csv:
+	writer = csv.writer(config_csv)
+	print(writer)
+	for line in lines:
+		# 行を適切に処理してCSVファイルに書き込みます
+		#print(line)
+		writer.writerow(line.strip().split())
