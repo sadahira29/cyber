0d83616b03c2494ecf3ac925f1eb5b7642069f2b
Reo Sadahira <s246116@wakayama.u.ac.jp>
2023-11-16 14:06:11 +0900

    snmpは一旦スルー routing-instancesの設定行の値を取得

--- test.py
+++ test.py
@@ -1,17 +1,11 @@
 import csv
 import re
 
-# 入力文字列
 # configを読み込む
 with open('config/juniper_config.txt', 'r') as config_txt:
 	# 各行を配列linesに格納
 	lines = config_txt.readlines()
 
-interfaces = []
-description = []
-unit = []
-family = []
-vlan_members = []
 
 for line in lines:
     line = line.lstrip("set").strip()
@@ -19,17 +13,28 @@ for line in lines:
         print(line)
         match = re.match(r"interfaces (\S+) description (.+)$", line)
         if match:
-            interfaces.append(match.group(1))
-            description.append(match.group(2))
-            print(match.group(1))
-            print(match.group(2))
+            interfaces = match.group(1)
+            description = match.group(2)
+            print(f"Interfaces: {interfaces}, Description: {description}")
             continue
         match = re.match(r"interfaces \S+ unit (\d+) family (\S+) vlan members (.+)$", line)
         if match:
-            unit.append(match.group(1))
-            family.append(match.group(2))
-            vlan_members.append(match.group(3))
-            print(match.group(1))
-            print(match.group(2))
-            print(match.group(3))
+            unit = match.group(1)
+            family = match.group(2)
+            vlan_members = match.group(3)
+            print(f"Unit: {unit}, Family: {family}, Vlan Members: {vlan_members}")
             continue
+    elif line.startswith("snmp"): # 後回し
+        print(line)
+        # match = re.match(r"snmp (\S+) description (.+)$", line)
+        # if match:
+        continue
+    elif line.startswith("routing-instances"):
+        print(line)
+        match = re.match(r"routing-instances mgmt_junos routing-options static route (\S+) next-hop (\S+)", line)
+        if match:
+            static_route = match.group(1)
+            next_hop = match.group(2)
+            print(f"Destination: {static_route}, Next Hop: {next_hop}")
+            continue
+
