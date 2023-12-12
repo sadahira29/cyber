1d158d85ee2328b8b2d3d571e58a2bfd96fb44e7
Reo Sadahira <s246116@wakayama.u.ac.jp>
2023-12-12 16:25:21 +0900

    CSVからYamlに変換するプログラムの最新バージョン

--- csv_to_yaml/csv_to_yml.py
+++ csv_to_yaml/csv_to_yml.py
@@ -2,67 +2,41 @@ import csv
 import yaml #PyYAMLをインストールする必要あり
 
 # 辞書型からYAMLに変換し、yamlファイルに書き込む関数を定義
+# 引数１：辞書型データ，引数２：出力するYAMLファイルのパス
 def dict_to_yaml(dict_data, output):
     with open(output, 'w') as yml:
         yaml.dump(dict_data, yml, default_flow_style=False, sort_keys=False)
 
-# Step.1 辞書型データを直接定義
-# 辞書型データ
-dict_data = {
-    'interface': [
-        {'id': 1, 'name': 'IF-1'},
-        {'id': 2, 'name': 'IF-2'},
-        {'id': 3, 'name': 'IF-3', 'speed': 100, 'duplex': 'full'},
-        {'id': 4, 'name': 'IF-4', 'speed': 100, 'duplex': 'full'}
-    ],
-    'vlan': [
-        {'id': 1, 'name': 'DEFAULT_VLAN'},
-        {'id': 2, 'name': 'VLAN_92_seg'}
-    ],
-}
-# 辞書型からYAMLに変換
-# output = 'csv_to_yaml/output.yaml'
-# dict_to_yaml(dict_data, output)
-
-
-# Step.2 あらかじめキーを配列で用意
-def csv_to_yaml(dict_key, dict_data, input, output):
-    # CSVを読み込み、辞書型に変換する
+# CSVから辞書データに変換する関数の定義
+# 引数　：CSVファイルのパス
+# 返り値：CSVから変換した辞書型データ
+def csv_to_dict(input):
+    # 空の辞書データ
+    dict_data = {}
+    # CSVを読み込む
     with open(input, 'r', newline='') as f:
-        reader = csv.DictReader(f)
-        for key in dict_key:
-            dict_data[key] = []
-            for line in reader:
-                dict_data[key].append(line)
-    dict_to_yaml(dict_data, output)
+        reader = csv.reader(f)
+        # csvを1行ずつ読み込む
+        for row in reader:
+            # 配列(row)の長さが１ならば
+            if len(row) == 1:
+                # テーブル名として取得する
+                table_name = row[0]
+                # テーブル名に対する辞書データの定義（空の配列）
+                dict_data[table_name] = []
+                # 次の行はカラムの行（のはず）
+                columns_row = next(reader)
+                # 以降の処理をスキップして次のループへ
+                continue
+            # テーブル名に対応する辞書データをリストに追加
+            # zip(a,b) : リストa,bの対応する要素をタプルに変換 例：[(a1,b1),(a2,b2),...]
+            # dict(c)  : タプルの1番目の要素をキー、2番目の要素を値として辞書型に変換
+            dict_data[table_name].append(dict(zip(columns_row, row)))
+    return dict_data
 
-dict_key  = ['interface', 'vlan', 'routing']
-dict_data = {}
-input  = 'csv_to_yaml/csv/step2.csv'
-output = 'csv_to_yaml/yaml/output3.yaml'
 
-# csv_to_yaml(dict_key, dict_data, input, output)
+input  = 'csv_to_yaml/example.csv'
+output = 'csv_to_yaml/example.yaml'
 
-
-# Step.3 CSVからキーを抽出する
-dict_key = []
-dict_data = {}
-key_number = 0
-input  = 'csv_to_yaml/csv/step3.csv'
-output = 'csv_to_yaml/yaml/output3.yaml'
-
-# CSVを読み込む
-with open(input, 'r', newline='') as f:
-    csv_dict = []
-    reader = csv.reader(f)
-    for line in reader:
-        if len(line) == 1:
-            key = line[0]
-            dict_key.append(key)
-            dict_data[key] = []
-            header = next(reader)
-            key_number += 1
-            continue
-        dict_data[dict_key[key_number - 1]].append(dict(zip(header, line)))
-
+dict_data = csv_to_dict(input)
 dict_to_yaml(dict_data, output)
