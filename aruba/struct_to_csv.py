3a234f554cc286e8867a2eb87c536c897c01dc46
Reo Sadahira <s246116@wakayama.u.ac.jp>
2024-02-06 17:16:05 +0900

    タグ系を取得できるようにした。 サブネットマスクをどんな値でも変換できるようにした。

--- aruba/struct_to_csv.py
+++ aruba/struct_to_csv.py
@@ -1,5 +1,6 @@
 import csv
 import re
+import ipaddress
 
 #------------------------------------------------------
 # 関数の定義
@@ -45,7 +46,7 @@ vlan_headers      = ['VlanID', 'name', 'ip address', 'ip address mask']
 with open('./aruba/config/aruba-struct.txt', 'r', newline='') as file:
     # 1行ずつ読み込む
     for line in file:
-        # 両端の空白や改行文字を取り除く
+        # 両端の空白や改行行字を取り除く
         line = line.strip()
         # System (hostname) の抽出
         if line.startswith('hostname'):
@@ -53,16 +54,22 @@ with open('./aruba/config/aruba-struct.txt', 'r', newline='') as file:
             system_config.append([hostname])
         # interface の抽出
         elif line.startswith('interface'):
-            # 例：interface 1
             # interface 内の項目の初期化
             name = port = speed = duplex = negotiation = ip_address = ip_address_mask = untag = tag_start = tag_end = lacp_port = ''
+            tag_ranges = []
+            # 例：interface 1
             port = line.split(' ')[1]
             while line != 'exit':  # exit が来るまで繰り返す
-                # 次の文に行く
+                # 次の行に行く
                 line = next(file).strip()
                 if line.startswith('name'):
                     # 例：name "IF-1"
-                    name = line.split(' ')[1].replace('"', '')
+                    # name行の正規表現パターンの定義
+                    match = re.match(r'name "(.*?)"', line)
+                    # パターンがマッチした場合
+                    if match:
+                        name = match.group(1)
+                        continue
                 elif line.startswith('speed-duplex'):
                     # 例：speed-duplex 100-full
                     speed_duplex = line.split(' ')[1]
@@ -70,52 +77,48 @@ with open('./aruba/config/aruba-struct.txt', 'r', newline='') as file:
                     duplex = speed_duplex.split('-')[1]
                 elif line.startswith('tagged'):
                     # 例：tagged vlan 92,401-407,409-414,416-424,427
-                    tagged = line.split(' ')[2]
+                    tag_ranges = line.split(' ')[2].split(',')
                 elif line.startswith('untagged'):
                     # 例：untagged vlan 401
-                    untagged = line.split(' ')[2]
-            # 抽出した config をリスト化して追加
-            interface_config.append([name, port, speed, duplex, negotiation, ip_address, ip_address_mask, untag, tag_start, tag_end, lacp_port])
+                    untag = line.split(' ')[2]
+            if tag_ranges:  # 配列の中身がある場合は
+                # tagの範囲の数の分追加（他のconfigは同じ）
+                for tag_range in tag_ranges:
+                    if '-' in tag_range:  # 409-414 このように tag が範囲を表す場合は
+                        tag_start = tag_range.split('-')[0]  # 前半をスタート
+                        tag_end   = tag_range.split('-')[1]  # 公判をエンド
+                    else:  # 1つだけなら、スタートもエンドも同じ値にする
+                        tag_start = tag_end = tag_range
+                    # 抽出した config をリスト化して追加
+                    interface_config.append([name, port, speed, duplex, negotiation, ip_address, ip_address_mask, untag, tag_start, tag_end, lacp_port])
+            else:  # ない場合はそのまま追加
+                interface_config.append([name, port, speed, duplex, negotiation, ip_address, ip_address_mask, untag, tag_start, tag_end, lacp_port])
         elif line.startswith('vlan'):  # vlan で始まるなら
-            # 例：
+            # 例：vlan 1
             # 各項目の初期化
-            vlan_id = name = tagged = untagged = no_untag = ip_address = ip_address_mask = ''
+            vlan_id = name = ip_address = ip_address_mask = ''
             vlan_id = line.split(' ')[1]
-            # 次の文に行く
-            line = next(file).strip()
             while line != 'exit':  # exit が来るまで繰り返す
-                if match = re.match(r"name (\S+)", line):
+                # 次の行に行く
+                line = next(file).strip()
+                if line.startswith('name'):
                     # 例：name "DEFAULT_VLAN"
-                    # パターンマッチングのための正規表現パターンの定義
-                    match = re.match(r"name (\S+)", line)
+                     # name行の正規表現パターンの定義
+                    match = re.search(r'name "(.*?)"', line)
                     # パターンがマッチした場合
                     if match:
-                        static_route = match.group(1)
-                        next_hop = match.group(2)
-                        # print(f"Destination: {static_route}, Next Hop: {next_hop}")
+                        name = match.group(1)
                         continue
-                    name = line.split(' ')[1].replace('"', '')
-                elif line.startswith('no untagged'):
-                    # 例：no untagged 2-23,25-26,48
-                    no_untag = line.split(' ')[2]
-                elif line.startswith('untagged'):
-                    # 例：untagged 1,24,27-47,49-52
-                    untagged = line.split(' ')[1]
-                elif line.startswith('tagged'):
-                    # 例：tagged 1
-                    tagged = line.split(' ')[1]
-                elif line.find('ip address'):
-                    print(name)
-                    if line == 'no ip address':
-                        # 次の文に行く
-                        line = next(file).strip()
-                        continue
+                elif line.startswith('ip address'):
                     # 例：ip address 192.168.66.88 255.255.255.0
-                    ip_address = line.split(' ')[2]
-                    ip_address_mask = line.split(' ')[3]
-                    ip_address_mask = '24' if ip_address_mask == '255.255.255.0' else ip_address_mask
-                # 次の文に行く
-                line = next(file).strip()
+                    # IPアドレスとサブネットマスクを取得
+                    ip_address, subnet_mask = line.split()[2], line.split()[3]
+                    # IPv4アドレスオブジェクトを作成
+                    address_obj = ipaddress.IPv4Address(ip_address)
+                    # IPv4ネットワークオブジェクトを作成
+                    network_obj = ipaddress.IPv4Network(f'{address_obj}/{subnet_mask}', strict=False)
+                    # プレフィックス長を取得
+                    ip_address_mask = network_obj.prefixlen
 
             # 抽出した config をリスト化して追加
             vlan_config.append([vlan_id, name ,ip_address, ip_address_mask])
