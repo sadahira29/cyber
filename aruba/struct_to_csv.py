f556518807baf6e4113767ba9e45c4890847279a
Reo Sadahira <s246116@wakayama.u.ac.jp>
2024-02-07 14:54:28 +0900

    コメントの追加と細かい修正

--- aruba/struct_to_csv.py
+++ aruba/struct_to_csv.py
@@ -15,6 +15,16 @@ def convertCSV(csv_path, csv_headers, config_lists):
         for config_list in config_lists:
             writer.writerow(config_list)
 
+# サブネットマスクをプレフィックス長に変換する関数
+def getPrefixLen(ip_address, subnet_mask):
+    # IPv4アドレスオブジェクトを作成
+    address_obj = ipaddress.IPv4Address(ip_address)
+    # IPv4ネットワークオブジェクトを作成
+    network_obj = ipaddress.IPv4Network(f'{address_obj}/{subnet_mask}', strict=False)
+    # プレフィックス長を取得
+    prefix_len = network_obj.prefixlen
+    return prefix_len
+
 #------------------------------------------------------
 # 変数の定義
 #------------------------------------------------------
@@ -26,17 +36,42 @@ interface_csv_path = output_directry + 'Interface.csv'
 routing_csv_path   = output_directry + 'Routing.csv'
 vlan_csv_path      = output_directry + 'Vlan.csv'
 
-# 抽出したConfigurationを保存しておくリスト
-system_config    = []
-interface_config = []
-routing_config   = []
-vlan_config      = []
+# 抽出した config を保存しておくリスト
+system_config    = []  # システム
+interface_config = []  # インターフェース
+routing_config   = []  # ルーティング
+vlan_config      = []  # VLAN
 
-# 各configのheaderリスト
+# システム情報のヘッダー
 system_headers    = ['hostname']
-interface_headers = ['port', 'name', 'speed', 'duplex', 'negotiation', 'ip address', 'ip address mask', 'untag', 'tagged start', 'tagged end', 'lacp_port']
-routing_headers   = ['dest_ip', 'dest_mask', 'gw', 'metric']
-vlan_headers      = ['VlanID', 'name', 'ip address', 'ip address mask']
+# インターフェース情報のヘッダー
+interface_headers = [
+    'port',              # ポート番号
+    'name',              # インターフェースの名前
+    'speed',             # 通信速度
+    'duplex',            # デュプレックスモード
+    'negotiation',       # ネゴシエーション設定
+    'ip address',        # IPアドレス
+    'ip address mask',   # IPアドレスのサブネットマスク
+    'untag',             # VLANなしの場合の設定
+    'tagged start',      # タギングの開始VLAN
+    'tagged end',        # タギングの終了VLAN
+    'lacp_port'          # LACP（Link Aggregation Control Protocol）ポートの設定
+]
+# ルーティング情報のヘッダー
+routing_headers = [
+    'dest_ip',    # 宛先IPアドレス
+    'dest_mask',  # 宛先サブネットマスク
+    'gw',         # ゲートウェイ
+    'metric'      # メトリック
+]
+# VLAN情報のヘッダー
+vlan_headers = [
+    'VlanID',          # VLAN ID
+    'name',            # VLANの名前
+    'ip address',      # VLANのIPアドレス
+    'ip address mask'  # VLANのIPアドレスのサブネットマスク
+]
 
 #------------------------------------------------------
 # メインの処理
@@ -48,89 +83,81 @@ with open('./aruba/config/aruba-struct.txt', 'r', newline='') as file:
     for line in file:
         # 両端の空白や改行行字を取り除く
         line = line.strip()
-        # System (hostname) の抽出
+        # System  の config(hostname) 抽出
         if line.startswith('hostname'):
-            hostname = line.split(' ')[1].replace('"', '')
+            hostname = line.split()[1].replace('"', '')
             system_config.append([hostname])
-        # interface の抽出
-        elif line.startswith('interface'):
+        # interface の config 抽出
+        elif line.startswith('interface'):  # 例：interface 1
             # interface 内の項目の初期化
-            name = port = speed = duplex = negotiation = ip_address = ip_address_mask = untag = tag_start = tag_end = lacp_port = ''
+            name = port = speed = duplex = negotiation = ip_address = subnet_mask = prefix_len = untag = tag_start = tag_end = lacp_port = ''
+            # 複数のtagを格納するためのリスト
             tag_ranges = []
-            # 例：interface 1
-            port = line.split(' ')[1]
+            # ポートの抽出
+            port = line.split()[1]
             while line != 'exit':  # exit が来るまで繰り返す
                 # 次の行に行く
                 line = next(file).strip()
                 if line.startswith('name'):
                     # 例：name "IF-1"
-                    # name行の正規表現パターンの定義
-                    match = re.match(r'name "(.*?)"', line)
-                    # パターンがマッチした場合
-                    if match:
-                        name = match.group(1)
-                        continue
+                    # name行の正規表現の定義
+                    name = re.search(r'name "(.*?)"', line).group(1)
                 elif line.startswith('speed-duplex'):
                     # 例：speed-duplex 100-full
-                    speed_duplex = line.split(' ')[1]
+                    speed_duplex = line.split()[1]
+                    # 例：100-full
                     speed  = speed_duplex.split('-')[0]
                     duplex = speed_duplex.split('-')[1]
-                elif line.startswith('tagged'):
-                    # 例：tagged vlan 92,401-407,409-414,416-424,427
-                    tag_ranges = line.split(' ')[2].split(',')
                 elif line.startswith('untagged'):
                     # 例：untagged vlan 401
-                    untag = line.split(' ')[2]
-            if tag_ranges:  # 配列の中身がある場合は
-                # tagの範囲の数の分追加（他のconfigは同じ）
+                    untag = line.split()[2]
+                elif line.startswith('tagged'):
+                    # 例：tagged vlan 92,401-407,409-414,416-424,427
+                    tag_ranges = line.split()[2].split(',')
+            if tag_ranges:  # tag_ranges の中身がある場合は
+                # tagの範囲の数の分追加（他の config は同じ）
                 for tag_range in tag_ranges:
                     if '-' in tag_range:  # 409-414 このように tag が範囲を表す場合は
                         tag_start = tag_range.split('-')[0]  # 前半をスタート
-                        tag_end   = tag_range.split('-')[1]  # 公判をエンド
+                        tag_end   = tag_range.split('-')[1]  # 後半をエンド
                     else:  # 1つだけなら、スタートもエンドも同じ値にする
                         tag_start = tag_end = tag_range
-                    # 抽出した config をリスト化して追加
-                    interface_config.append([name, port, speed, duplex, negotiation, ip_address, ip_address_mask, untag, tag_start, tag_end, lacp_port])
+                    # 抽出した config をヘッダーに合わせた順番にリスト化して追加
+                    interface_config.append([name, port, speed, duplex, negotiation, ip_address, prefix_len, untag, tag_start, tag_end, lacp_port])
             else:  # ない場合はそのまま追加
-                interface_config.append([name, port, speed, duplex, negotiation, ip_address, ip_address_mask, untag, tag_start, tag_end, lacp_port])
-        elif line.startswith('vlan'):  # vlan で始まるなら
-            # 例：vlan 1
+                interface_config.append([name, port, speed, duplex, negotiation, ip_address, prefix_len, untag, tag_start, tag_end, lacp_port])
+        # vlan の config 抽出
+        elif line.startswith('vlan'):  # 例：vlan 1
             # 各項目の初期化
-            vlan_id = name = ip_address = ip_address_mask = ''
-            vlan_id = line.split(' ')[1]
+            vlan_id = vlan_name = ip_address = subnet_mask = prefix_len = ''
+            # vlanIDの抽出
+            vlan_id = line.split()[1]
             while line != 'exit':  # exit が来るまで繰り返す
                 # 次の行に行く
                 line = next(file).strip()
-                if line.startswith('name'):
-                    # 例：name "DEFAULT_VLAN"
-                     # name行の正規表現パターンの定義
+                if line.startswith('name'):  # 例：name "DEFAULT_VLAN"
+                    # name行の正規表現パターンの定義
                     match = re.search(r'name "(.*?)"', line)
                     # パターンがマッチした場合
                     if match:
-                        name = match.group(1)
-                        continue
-                elif line.startswith('ip address'):
-                    # 例：ip address 192.168.66.88 255.255.255.0
-                    # IPアドレスとサブネットマスクを取得
-                    ip_address, subnet_mask = line.split()[2], line.split()[3]
-                    # IPv4アドレスオブジェクトを作成
-                    address_obj = ipaddress.IPv4Address(ip_address)
-                    # IPv4ネットワークオブジェクトを作成
-                    network_obj = ipaddress.IPv4Network(f'{address_obj}/{subnet_mask}', strict=False)
-                    # プレフィックス長を取得
-                    ip_address_mask = network_obj.prefixlen
-
-            # 抽出した config をリスト化して追加
-            vlan_config.append([vlan_id, name ,ip_address, ip_address_mask])
-        elif line.startswith('ip route'):
-            # 例：ip route 10.6.1.0 255.255.255.0 10.5.16.250
-            dest_ip = dest_mask = gateway = metric = ''
-            dest_ip   = line.split(' ')[2]
-            dest_mask = line.split(' ')[3]
-            gateway   = line.split(' ')[4]
-            dest_mask = '24' if dest_mask == '255.255.255.0' else dest_mask
-            # 抽出した config をリスト化して追加
-            routing_config.append([dest_ip, dest_mask, gateway, metric])
+                        vlan_name = match.group(1)
+                # IPアドレスとサブネットマスク（プレフィックス長）の抽出
+                elif line.startswith('ip address'):  # 例：ip address 192.168.66.88 255.255.255.0
+                    ip_address  = line.split()[2]  # IPアドレス
+                    subnet_mask = line.split()[3]  # サブネットマスク
+                    prefix_len  = getPrefixLen(ip_address, subnet_mask)  # プレフィックス長
+            # 抽出した config をヘッダーに合わせた順番にリスト化して追加
+            vlan_config.append([vlan_id, vlan_name ,ip_address, prefix_len])
+        # routing の config 抽出
+        elif line.startswith('ip route'):  # 例：ip route 10.6.1.0 255.255.255.0 10.5.16.250
+            # 各項目の初期化
+            ip_address = subnet_mask = prefix_len = gateway = metric = ''
+            ip_address  = line.split()[2]  # IPアドレス
+            subnet_mask = line.split()[3]  # サブネットマスク
+            gateway     = line.split()[4]  # ゲートウェイ
+            prefix_len  = getPrefixLen(ip_address, subnet_mask)  # プレフィックス長
+            # 抽出した config をヘッダーに合わせた順番にリスト化して追加
+            routing_config.append([ip_address, prefix_len, gateway, metric])
 
 
 # 抽出した各Configuration をCSVに変換する
