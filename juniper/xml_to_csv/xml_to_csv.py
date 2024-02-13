b7133d924fc6385c54adb38c07551d292258a92b
Reo Sadahira <s246116@wakayama.u.ac.jp>
2024-02-13 17:02:41 +0900

    juniper ポートモードによるuntag, tagの切り替えを実装。 仮にではあるがipアドレスの抽出も実装。

--- juniper/xml_to_csv/xml_to_csv.py
+++ juniper/xml_to_csv/xml_to_csv.py
@@ -7,13 +7,20 @@ import xml.etree.ElementTree as ET
 #------------------------------------------------------
 
 # リストからCSVに変換する関数
-def convertCSV(csv_path, csv_headers, config_lists):
+def export_list_to_csv(csv_path, csv_headers, config_lists):
     with open(csv_path, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(csv_headers)
         # リストを1つずつCSVに変換し書き込む
         for config_list in config_lists:
             writer.writerow(config_list)
+
+def get_vlan_id(member_name, vlans):
+    for vlan in vlans:
+        vlan_name = vlan.find('name').text
+        # メンバとvlan名が一致した時idの取得
+        if  member_name.text == vlan_name:
+            return vlan.find('vlan-id').text
 
 #------------------------------------------------------
 # 変数の定義
@@ -117,37 +124,57 @@ if __name__ == '__main__':
     for interface in interfaces:
         # 各変数の初期化（ループするたびに初期化する）
         name = port = speed = duplex = negotiation = ip_address = subnet_mask = untag = tag_start = tag_end = lacp_port = ''
-        member = ''
+        vlan_tags = []
 
+        # interfaceのタイプからポート番号を抽出
         current_tag = interface.find('name').text.split('/')
-        IF_type = current_tag[0]
-        if IF_type.startswith('ge'):
-            # juniperではポート番号から０から始まる？
+        interface_type = current_tag[0]
+        if interface_type.startswith('ge'):
+            # juniperではポート番号から０から始まるので
             # arubaに合わせるために＋１している
             port = int(current_tag[2]) + 1
         else:  # me0 はとりあえず無視している
             continue
+        # description の抽出
         current_tag = interface.find('description')
         # is not Noneを書かないと指定したタグがない時にエラーになる
         if current_tag is not None:
             name = current_tag.text
+        # スピードの抽出
         current_tag = interface.find('ether-options/speed')
         if current_tag is not None:
             speed  = current_tag[0].tag.split('-')[1]
-            duplex = 'full'  # full であると勝手に仮定している
+            duplex = 'full'  # full であると仮定
+        # tag, untagの抽出（推測）
         current_tag = interface.find('.//ethernet-switching')
         if current_tag is not None:
-            if current_tag[0].text == 'access':
-                member = current_tag.find('vlan/members').text
-                vlans = root.findall('.//vlans/vlan')
-                for vlan in vlans:
-                    vlan_name = vlan.find('name').text
-                    if vlan.find('name').text == member:
-                        untag = vlan.find('vlan-id').text
+            vlans = root.findall('.//vlans/vlan')
+            port_mode = current_tag[0].text
+            # port_modeが'access'のとき，untagを取得
+            if port_mode == 'access':
+                member_name = current_tag.find('vlan/members')
+                untag = get_vlan_id(member_name, vlans)
+            # port_modeが'access'のとき，untagを取得
+            elif port_mode == 'trunk':
+                member_names = current_tag.findall('vlan/members')
+                # メンバ(vlan名)に対するvlan idを取得する
+                for member_name in member_names:
+                    vlan_tags.append(get_vlan_id(member_name, vlans))
+        # ip adressの抽出（仮定）
+        current_tag = interface.find('.//inet/address')
+        if current_tag is not None:
+            ip_adress_cidr = current_tag.find('name').text.split('/')
+            ip_address = ip_adress_cidr[0]
+            subnet_mask = ip_adress_cidr[1]
+        # tagの割り当て
+        if vlan_tags:
+            for vlan_tag in vlan_tags:
+                tag_start = tag_end = vlan_tag
+                # 抽出した config をヘッダーの順番に合わせてリスト化して追加
+                interface_config.append([device_id, port, name, speed, duplex, negotiation, ip_address, subnet_mask, untag, tag_start, tag_end, lacp_port])
+        else:
+            interface_config.append([device_id, port, name, speed, duplex, negotiation, ip_address, subnet_mask, untag, tag_start, tag_end, lacp_port])
 
-
-        # 抽出した config をヘッダーの順番に合わせてリスト化して追加
-        interface_config.append([device_id, port, name, speed, duplex, negotiation, ip_address, subnet_mask, untag, tag_start, tag_end, lacp_port])
     # RoutingのConfiguration抽出
     # XML例：
         # <route>
@@ -185,8 +212,8 @@ if __name__ == '__main__':
         # 抽出した config をヘッダーの順番に合わせてリスト化して追加
         vlan_config.append([device_id, vlan_id, vlan_desc, ip_address, subnet_mask])
 
-    # 抽出した Configuration をCSVに変換する
-    convertCSV(system_csv_path, system_headers, system_config)           # System
-    convertCSV(interface_csv_path, interface_headers, interface_config)  # Interface
-    convertCSV(routing_csv_path, routing_headers, routing_config)        # Routing
-    convertCSV(vlan_csv_path, vlan_headers, vlan_config)                 # Valn
+    # 抽出した Configuration リストをCSVに変換し出力する
+    export_list_to_csv(system_csv_path, system_headers, system_config)           # System
+    export_list_to_csv(interface_csv_path, interface_headers, interface_config)  # Interface
+    export_list_to_csv(routing_csv_path, routing_headers, routing_config)        # Routing
+    export_list_to_csv(vlan_csv_path, vlan_headers, vlan_config)                 # Valn
