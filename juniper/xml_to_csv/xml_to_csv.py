1a126d659d7ab11240a3ba7368757e3d67558ff0
Reo Sadahira <s246116@wakayama.u.ac.jp>
2024-02-14 17:13:32 +0900

    xml_to_csvのコードの整理（関数化や変数名の変更など）

--- juniper/xml_to_csv/xml_to_csv.py
+++ juniper/xml_to_csv/xml_to_csv.py
@@ -1,32 +1,11 @@
+import os
 import csv
 import xml.etree.ElementTree as ET
 
-
-#------------------------------------------------------
-# 関数の定義
-#------------------------------------------------------
-
-# リストからCSVに変換する関数
-def list_to_csv(csv_path, csv_headers, config_lists):
-    with open(csv_path, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(csv_headers)
-        # リストを1つずつCSVに変換し書き込む
-        for config_list in config_lists:
-            writer.writerow(config_list)
-
-def get_vlan_id(member_name, vlans):
-    for vlan in vlans:
-        vlan_name = vlan.find('name').text
-        # メンバとvlan名が一致した時idの取得
-        if  member_name.text == vlan_name:
-            return vlan.find('vlan-id').text
-
-#------------------------------------------------------
-# 変数の定義
-#------------------------------------------------------
-
-# outputするCSVファイルのパス
+#======================================================
+# グローバル変数の定義
+#======================================================
+# 出力するCSVファイルのパス
 output_directry    = './output/'
 system_csv_path    = output_directry + 'System_ju.csv'
 interface_csv_path = output_directry + 'Interface_ju.csv'
@@ -34,9 +13,9 @@ routing_csv_path   = output_directry + 'Routing_ju.csv'
 vlan_csv_path      = output_directry + 'Vlan_ju.csv'
 
 # 抽出した config を保存しておくリスト
-system_config    = []   # システム
-interface_config = []   # インターフェース
-routing_config   = []   # ルーティング
+system_config    = []   # System
+interface_config = []   # Interface
+routing_config   = []   # Routing
 vlan_config      = []   # VLAN
 
 # システム情報のヘッダー
@@ -76,131 +55,123 @@ vlan_headers = [
     'ip address mask'   # VLANのIPアドレスのサブネットマスク
 ]
 
-#------------------------------------------------------
-# メインの処理
-#------------------------------------------------------
-if __name__ == '__main__':
-    # コマンドライン引数として機器IDを取得
-    device_id = input('Enter the decice ID: ')
-    # 変換対象のaruba configのファイル名（パス）を取得
-    juniper_config_xml = input("Enter the file name or path of juniper config(XML): ")
-    # XMLを扱えるようにするための準備
-    tree = ET.parse(juniper_config_xml)
-    root = tree.getroot()
+# 識別子としての機器ID
+device_id = ''
 
-    # System(管理情報)のConfiguration抽出
-    # 指定したXPathに対応する子ノードの抽出
-    system_hostname = root.find(".//system/host-name")
-    if system_hostname is not None:
-        hostname = system_hostname.text
+#======================================================
+# 関数の定義
+#======================================================
+
+# ファイル名を取得する関数
+def get_xml_file():
+    while True:
+        file_name = input("Enter the file name of juniper config (.xml): ")
+        _, file_extension = os.path.splitext(file_name)
+        # 拡張子が ".xml" でない場合の処理
+        if file_extension != ".xml":
+            print("Error: The file extension is not .xml. Please enter the file with .xml extension.")
+        else:
+            break
+    return file_name
+
+# System(管理情報)の config を抽出する関数
+def extract_system_config(root):
+    global system_config
+    hostname = ''
+    # XPathでhost-nameタグを検索
+    hostname_tag = root.find(".//system/host-name")
+    if hostname_tag is not None:
+        hostname = hostname_tag.text
         # 抽出した config をヘッダーの順番に合わせてリスト化して追加
         system_config.append([hostname, device_id])
-    # InterfaceのConfiguration抽出
-    # XML例：
-        # <interface>
-        #     <name>ge-0/0/20</name>
-        #     <description>IF-21</description>
-        #     <ether-options>
-        #         <speed>
-        #             <ethernet-100m/>
-        #         </speed>
-        #     </ether-options>
-        #     <unit>
-        #         <name>0</name>
-        #         <family>
-        #             <ethernet-switching>
-        #                 <port-mode>access</port-mode>
-        #                 <vlan>
-        #                     <members>vlan421</members>
-        #                 </vlan>
-        #             </ethernet-switching>
-        #         </family>
-        #     </unit>
-        # </interface>
-    # 指定したXPathに対応する子ノードの抽出
+
+# Interfaceの config 抽出する関数
+def extract_interface_config(root):
+    global interface_config
+    # XPathでinterfacesタグを検索
     XPath = './/interfaces/interface'
-    # interfaceタグの子ノードをすべて抽出
+    # 子ノードのinterfaceタグをすべて取得
     interfaces = root.findall(XPath)
+    # interfaceタグ内の必要な設定を抽出して１つのリストに格納し、
+    # そのリストを1つずつinterface_configに格納する
     for interface in interfaces:
         # 各変数の初期化（ループするたびに初期化する）
         name = port = speed = duplex = negotiation = ip_address = subnet_mask = untag = tag_start = tag_end = lacp_port = ''
+        # 複数タグを保存するリスト
         vlan_tags = []
 
-        # interfaceのタイプからポート番号を抽出
-        current_tag = interface.find('name').text.split('/')
-        interface_type = current_tag[0]
+        # interface名からポート番号を抽出
+        interface_name = interface.find('name').text.split('/')
+        interface_type = interface_name[0]
         if interface_type.startswith('ge'):
             # juniperではポート番号から０から始まるので
             # arubaに合わせるために＋１している
-            port = int(current_tag[2]) + 1
+            port = int(interface_name[2]) + 1
         else:  # me0 はとりあえず無視している
             continue
+
         # description の抽出
-        current_tag = interface.find('description')
+        description_tag = interface.find('description')
         # is not Noneを書かないと指定したタグがない時にエラーになる
-        if current_tag is not None:
-            name = current_tag.text
+        if description_tag is not None:
+            name = description_tag.text
         # スピードの抽出
-        current_tag = interface.find('ether-options/speed')
-        if current_tag is not None:
-            speed  = current_tag[0].tag.split('-')[1]
+        speed_tag = interface.find('ether-options/speed')
+        if speed_tag is not None:
+            speed  = speed_tag[0].tag.split('-')[1]
             duplex = 'full'  # full であると仮定
         # tag, untagの抽出（推測）
-        current_tag = interface.find('.//ethernet-switching')
-        if current_tag is not None:
+        switching_tag = interface.find('.//ethernet-switching')
+        if switching_tag is not None:
             vlans = root.findall('.//vlans/vlan')
-            port_mode = current_tag[0].text
+            port_mode = switching_tag[0].text
             # port_modeが'access'のとき，untagを取得
             if port_mode == 'access':
-                member_name = current_tag.find('vlan/members')
+                member_name = switching_tag.find('vlan/members')
                 untag = get_vlan_id(member_name, vlans)
             # port_modeが'access'のとき，untagを取得
             elif port_mode == 'trunk':
-                member_names = current_tag.findall('vlan/members')
+                member_names = switching_tag.findall('vlan/members')
                 # メンバ(vlan名)に対するvlan idを取得する
                 for member_name in member_names:
                     vlan_tags.append(get_vlan_id(member_name, vlans))
         # ip adressの抽出（仮定）
-        current_tag = interface.find('.//inet/address')
-        if current_tag is not None:
-            ip_adress_cidr = current_tag.find('name').text.split('/')
+        address_tag = interface.find('.//inet/address')
+        if address_tag is not None:
+            # CIDR表記のIPアドレスを'/'で宛先アドレスとマスクに分ける
+            ip_adress_cidr = address_tag.find('name').text.split('/')
             ip_address = ip_adress_cidr[0]
             subnet_mask = ip_adress_cidr[1]
-        # tagの割り当て
+        # tagの割り当てと interface_config への格納
         if vlan_tags:
             for vlan_tag in vlan_tags:
+                # 開始タグと終了タグの設定
                 tag_start = tag_end = vlan_tag
                 # 抽出した config をヘッダーの順番に合わせてリスト化して追加
                 interface_config.append([device_id, port, name, speed, duplex, negotiation, ip_address, subnet_mask, untag, tag_start, tag_end, lacp_port])
         else:
+            # 抽出した config をヘッダーの順番に合わせてリスト化して追加
             interface_config.append([device_id, port, name, speed, duplex, negotiation, ip_address, subnet_mask, untag, tag_start, tag_end, lacp_port])
 
-    # RoutingのConfiguration抽出
-    # XML例：
-        # <route>
-        #     <name>10.6.1.0/24</name>
-        #     <next-hop>10.5.16.250</next-hop>
-        # </route>
-
+# Routing の config 抽出する関数
+def extract_routing_config(root):
+    global routing_config
     # routeタグの子ノードをすべて抽出
     XPath = './/routing-options/static/route'
     routes = root.findall(XPath)
     for route in routes:
         # 各変数の初期化（ループするたびに初期化する）
         dest_ip = dest_mask = gateway = metric = ''
-        # '/'で宛先アドレスとマスクに分ける
-        dest_ip = route.find('name').text.split('/')[0]
-        dest_mask = route.find('name').text.split('/')[1]
+        # CIDR表記のIPアドレスを'/'で宛先アドレスとマスクに分ける
+        ip_adress_cidr = route.find('name').text.split('/')
+        dest_ip = ip_adress_cidr[0]
+        dest_mask = ip_adress_cidr[1]
         gateway = route.find('next-hop').text
         # 抽出した config をヘッダーの順番に合わせてリスト化して追加
         routing_config.append([device_id, dest_ip, dest_mask, gateway, metric])
-    # VlanのConfiguration抽出
-        # XML例：
-        # <vlan>
-        #     <name>vlan1</name>
-        #     <description>DEFAULT_VLAN</description>
-        #     <vlan-id>1</vlan-id>
-        # </vlan>
+
+# Vlan の config 抽出する関数
+def extract_vlan_config(root):
     # vlanタグの子ノードをすべて抽出
     XPath = './/vlans/vlan'
     vlans = root.findall(XPath)
@@ -212,8 +183,51 @@ if __name__ == '__main__':
         # 抽出した config をヘッダーの順番に合わせてリスト化して追加
         vlan_config.append([device_id, vlan_id, vlan_desc, ip_address, subnet_mask])
 
-    # 抽出した Configuration リストをCSVに変換し出力する
-    list_to_csv(system_csv_path, system_headers, system_config)           # System
+# メンバ名（vlan名）と一致するvlan idを取得する関数
+def get_vlan_id(member_name, vlans):
+    for vlan in vlans:
+        vlan_name = vlan.find('name')
+        # メンバとvlan名が一致した時idの取得
+        if  member_name.text == vlan_name.text:
+            return vlan.find('vlan-id').text
+
+# リストからCSVに変換する関数
+def list_to_csv(csv_path, csv_headers, config_lists):
+    with open(csv_path, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
+        # リストを1つずつCSVに変換し書き込む
+        for config_list in config_lists:
+            writer.writerow(config_list)
+
+# メイン関数
+def main():
+    # グローバル変数の宣言
+    global device_id
+    # 入力された機器IDを取得
+    device_id = input('Enter the decice ID: ')
+    # 変換対象のjuniperのconfigのファイル名（パス）を取得
+    juniper_config_xml = get_xml_file()
+
+    # XMLを扱えるようにするための準備
+    tree = ET.parse(juniper_config_xml)
+    root = tree.getroot()
+
+    # 各項目の config の抽出
+    extract_system_config(root)     # System(管理情報)
+    extract_interface_config(root)  # Interface
+    extract_routing_config(root)    # Routing
+    extract_vlan_config(root)       # Vlan
+
+    # 抽出した  config  リストをCSVに変換し出力する
+    list_to_csv(system_csv_path, system_headers, system_config)           # System(管理情報)
     list_to_csv(interface_csv_path, interface_headers, interface_config)  # Interface
     list_to_csv(routing_csv_path, routing_headers, routing_config)        # Routing
-    list_to_csv(vlan_csv_path, vlan_headers, vlan_config)                 # Valn
+    list_to_csv(vlan_csv_path, vlan_headers, vlan_config)                 # Vlan
+
+#======================================================
+# プログラムの起点
+#======================================================
+
+if __name__ == '__main__':
+    main()
