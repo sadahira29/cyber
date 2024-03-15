9c8b8b4f572415f2060ffb2eca97b0a87abc159c
Naruki MITSUDA <manda@wakayama-u.ac.jp>
2024-03-15 16:08:32 +0900

    vlan先に修正

--- ansible/csv_to_yaml.py
+++ ansible/csv_to_yaml.py
@@ -3,8 +3,13 @@ import yaml #PyYAMLをインストールする必要あり
 import os
 import sys
 
-directory_path = sys.argv[1]
-yaml_file_path = sys.argv[2]
+#------------------------------------------------------
+# コマンドライン引数
+# Usage: csv_to_yaml.py [device ID] [csvfile directory path] [output yamlfile]
+#------------------------------------------------------
+device_id = sys.argv[1]
+directory_path = sys.argv[2]
+yaml_file_path = sys.argv[3]
 
 #------------------------------------------------------
 # ここから関数の定義
@@ -17,7 +22,7 @@ def csv_to_dict(csv_file_path, dict_data, config_name):
         # CSVを辞書型に変換する
         reader = csv.DictReader(f)
         # config名に対応する辞書型データの各行をリストに格納
-        dict_data[config_name] = [row for row in reader]
+        dict_data[config_name] = [row for row in reader if row["deviceID"] == device_id]
 
 # 辞書型からYAMLに変換し、yamlファイルに出力する関数
 def dict_to_yaml(dict_data, yaml_file_path):
@@ -39,11 +44,13 @@ dict_data = {}
 # 各CSVファイルに対してYaml変換を行い，全体の辞書型データを作成する
 for csv_file in csv_files:
     # 拡張子なしのファイル名をconfig名として取得する
-    config_name = os.path.splitext(os.path.basename(csv_file))[0]
-    # CSVファイルの相対パスを作成する
-    csv_file_path = directory_path + '/' + csv_file
-    # CSVから辞書型に変換し，辞書型変数に書き込む
-    csv_to_dict(csv_file_path, dict_data, config_name)
+    config_name, ext = os.path.splitext(os.path.basename(csv_file))
+    # 拡張子がcsvの場合のみ処理
+    if ext == ".csv":
+        # CSVファイルの相対パスを作成する
+        csv_file_path = directory_path + '/' + csv_file
+        # CSVから辞書型に変換し，辞書型変数に書き込む
+        csv_to_dict(csv_file_path, dict_data, config_name)
 
 # 作成した辞書型データをYamlに変換し，yamlファイルに出力する
 dict_to_yaml(dict_data, yaml_file_path)
